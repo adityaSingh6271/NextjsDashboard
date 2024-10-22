e46d59cc971ad66f603253dd3faf70a59958b578
Aditya Singh <singhaditya6271@gmail.com>
2024-10-22 20:44:28 +0530

    Updated dashboard components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css';
+import { inter } from 'app/ui/font'; // Assuming 'inter' is an object with a className or similar export
+import SideNav from './ui/dashboard/sidenav';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}> {/* Assuming inter has a className property */}
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      {/*<div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>*/}
+    </div>
+      </body>
     </html>
   );
 }
